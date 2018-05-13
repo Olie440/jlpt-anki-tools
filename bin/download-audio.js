798d396f22630025becf896e4fe200cbfbe5e5c3
Oliver Rushworth <olie440@msn.com>
2018-05-13 17:13:37 +0100

    Improve download audio scripts
    - Better error handling
    - Better reporting
    - Adds sounds to deck

--- bin/download-audio.js
+++ bin/download-audio.js
@@ -1,3 +1,5 @@
+const { writeFileSync, readdirSync } = require('fs');
+
 const anki = require('../lib/anki.js');
 const japanesepod101 = require('../lib/japanesepod101.js');
 const utils = require('../lib/utils.js');
@@ -5,8 +7,38 @@ const utils = require('../lib/utils.js');
 const deckPath = process.argv[2];
 const outputDir = process.argv[3];
 
-const words = anki.loadFile(deckPath)
+const words = anki.loadFile(deckPath);
+console.log('[✓] Loaded Deck');
 
-utils.concurrentInvoke(words, 10, (word) => {
-    return japanesepod101.downloadAudio(word.kana, outputDir);
+const download = utils.concurrentInvoke(words, 10, (word) => {
+    return japanesepod101.downloadAudio(word.kana, outputDir)
+        .then(() => {
+            console.log(`[✓] Audio for ${word.kana} downloaded`);
+        })
+        .catch(() => {
+            console.log(`[✘] Audio for ${word.kana} unavailable`);
+        });
+});
+
+download.then(() => {
+    console.log('[✓] Audio for all words downloaded');
+
+    const files = readdirSync(outputDir);
+    const output = words.map(word => {
+        const hasAudio = files.includes(`${word.kana}.mp3`);
+
+        if (hasAudio) {
+            word.raw.push(`[sound:${word.kana}.mp3]`);
+        } else {
+            word.raw.push('');
+        }
+
+        return word.raw.join('\t');
+    });
+
+    console.log('[✓] Audio added to end of deck data');
+
+    writeFileSync(outputDir + '/deck.txt', output.join('\n'));
+
+    console.log('[✓] Deck written to file');
 });
