f5edf59577158cb64244daf6f9c4680c4ded9f29
Oliver Rushworth <oliver@oliverushworth.uk>
2018-06-11 20:15:00 +0100

    Add create verb script

---
+++ bin/create-conjugation-deck.js
@@ -0,0 +1,32 @@
+const { writeFileSync } = require('fs');
+const _ = require('lodash');
+const dictionary = require('../output/dictionary.json');
+
+function createDeckRow(word) {
+    const row = [
+        word.forms.dictionary,
+        word.forms.furigana,
+        word.edictCode,
+        word.types[0],
+        word.conjugations.negative,
+        word.conjugations.polite,
+        word.conjugations.te
+    ];
+
+    return row.join(';');
+}
+
+const outputPath = process.argv[3];
+
+if (!outputPath) {
+    console.log('Usage: node create-conjugation-deck.js [Output Path]');
+    process.exit(1);
+}
+
+const result = _.chain(dictionary)
+    .filter('edictCode')
+    .map(createDeckRow)
+    .join('\n')
+    .valueOf();
+
+writeFileSync(outputPath, result);
