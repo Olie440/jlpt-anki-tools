3c2e04d9171c763b0ea06347130ad6b98bc7fb1e
Oliver Rushworth <oliver@oliverushworth.uk>
2018-08-19 16:08:42 +0100

    [Utils] - Update JapanesePod101 to take standard words

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const jisho = require('./lib/jisho.js');
+const word = process.argv[2];
 
-jisho.findWordForLevel('nomu', 'n5').then(console.log);
+jisho.findWordForLevel(word, 'n5').then(console.log);
